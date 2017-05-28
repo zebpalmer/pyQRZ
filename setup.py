f1e238f94a7e767e88095527a312f4758d690704
Zeb Palmer <zeb@zebpalmer.com>
2017-05-28 16:48:53 -0600

    classifiers

--- setup.py
+++ setup.py
@@ -18,19 +18,19 @@ setup(
     use_2to3=True,
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 4 - Beta'
-        'Intended Audience :: Developers'
-        'License :: OSI Approved :: MIT License'
-        'Natural Language :: English'
-        'Programming Language :: Python'
-        'Programming Language :: Python :: 2'
-        'Programming Language :: Python :: 2.7'
-        'Programming Language :: Python :: 3'
-        'Programming Language :: Python :: 3.4'
-        'Programming Language :: Python :: 3.5'
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3 :: Only'
-        'Topic :: Communications :: Ham Radio'
-        'Topic :: Software Development :: Libraries'
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Communications :: Ham Radio',
+        'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules'],
 )
